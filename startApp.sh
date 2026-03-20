7e68421e01a9142bf92d030011ea5fdb30ea3f2f
Debjit Chattopadhyay <debjitchatterjee31@gmail.com>
2026-03-21 01:43:27 +0530

    db creds for docker deployment

--- startApp.sh
+++ startApp.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-echo "🚧 Stopping all running Docker containers..."
-docker stop $(docker ps -q) 2>/dev/null
-
-echo "🗑️ Removing all Docker containers..."
-docker rm $(docker ps -aq) 2>/dev/null
-
-echo "🔥 Removing only student-management-backend images..."
-docker rmi -f $(docker images -q --filter=reference='*student-management-backend*') 2>/dev/null
-
 echo "⚡ Running Maven build..."
 mvn clean package -DskipTests
 
